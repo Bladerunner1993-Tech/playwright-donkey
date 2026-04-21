444bdd7c8fe405e7dbd4d32fabc0213d1990bcf4
Bladerunner1993-Tech <leksometreveli93@gmail.com>
2026-04-21 07:48:46 +0200

    Break password on purpose to test CI failure

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('user can login to SauceDemo', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.getByPlaceholder('Username').fill('standard_user');
-  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByPlaceholder('Password').fill('WRONG_PASSWORD_HAHA');
   await page.getByRole('button', { name: 'Login' }).click();
 
   await expect(page).toHaveURL(/inventory/);
